9ed8fc170c3ce80bee500cfd2d5d81b8715507e1
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-12-29 17:17:05 -0500

    initializing repo

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
