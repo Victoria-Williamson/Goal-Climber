74dbc94bc4ceb6b95fbddf13f71c223218df7b12
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-12-29 17:27:40 -0500

    changing backend build

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static(path.join(__dirname, "..", "web", "build")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", "web", "build", "index.html"));
-  });
-}
+// // Serve static assets if in production
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static(path.join(__dirname, "..", "web", "build")));
+//   app.get("/*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "..", "web", "build", "index.html"));
+//   });
+// }
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
