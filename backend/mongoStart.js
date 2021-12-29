dd3a7fcd370303321171af48bca3000f9c850759
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-12-29 17:43:10 -0500

    added mongodb

---
+++ backend/mongoStart.js
@@ -0,0 +1,29 @@
+const mongoClient = require("mongodb").MongoClient;
+
+var _db;
+
+function connect(callback) {
+  mongoClient.connect(
+    process.env.DATABASE_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, db) => {
+      _db = db;
+      console.log("mongo connected");
+      callback();
+    }
+  );
+}
+
+function get() {
+  return _db;
+}
+
+function close() {
+  _db.close();
+}
+
+module.exports = {
+  connect,
+  get,
+  close,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
+const mongoUtil = require("./mongoStart.js");
+
+const PORT = process.env.PORT || 5005;
+
 const app = express();
+app.set("port", PORT);
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// // Serve static assets if in production
-// if (process.env.NODE_ENV === "production") {
-//   // Set static folder
-//   app.use(express.static(path.join(__dirname, "..", "web", "build")));
-//   app.get("/*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "..", "web", "build", "index.html"));
-//   });
-// }
+app.use(express.static(path.join(__dirname, "build")));
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+mongoUtil.connect(() => {
+  app.listen(PORT, () => console.log("Server listening on port " + PORT));
 });
