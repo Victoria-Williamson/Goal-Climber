1f99f1243b56f4322265e98194fb5d409066afb4
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-12-29 17:23:38 -0500

    changing build location

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "../build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build"));
-});
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, "..", "web", "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "web", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
