919f6c6cf1ee103fb7be71213cc40fceee3e029f
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-12-29 20:56:16 -0500

    improving scritps

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "build")));
+app.get("/", (req, res) => {
+  res.send("Project Home");
+});
 
-const AuthAPI = require("./api/auth");
-app.use("/auth", AuthAPI);
-
-// app.get("/", function (req, res) {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
 mongoUtil.connect(() => {
   app.listen(PORT, () => console.log("Server listening on port " + PORT));
 });
